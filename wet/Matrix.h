5406346a61d21de5c1b4fd9644134fdcc76b6219
Ron <ron.tzabari@campus.technion.ac.il>
2026-06-07 17:51:14 +0300

    Add c'tors and d'tor, Implement get/set functions, Implement functions for parts 1-4 (4.3)

--- wet/Matrix.h
+++ wet/Matrix.h
@@ -5,6 +5,34 @@
 class Matrix{
     int n; //Rows of matrix
     int m; //Cullomns of matrix
-    int *mat_ptr = new Matrix[n*m];
+    int len = n * m;
+    int *mat_ptr = new Matrix[len];
 
+    public:
+        //without third argument
+        Matrix(int n, int m, int initial){
+            for(int i = 0; i < len - 1; i++){
+                mat_ptr[i] = initial;
+            }
+        }
+        //with third argument
+        Matrix(int n, int m){
+            for(int i = 0; i < len - 1; i++){
+                mat_ptr[i] = 0;
+            }
+        }
+        //default c'tor
+        Matrix() : n(0), m(0){}
+
+        //copy c'tor
+        Matrix(Matrix& mat){
+            n = mat.n;
+            m = mat.m;
+            *mat_ptr = new Matrix[len];
+        }
+
+        //destructor
+        ~Matrix(){
+            delete[];
+        }
 };
