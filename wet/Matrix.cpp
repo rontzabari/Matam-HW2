0370823d9d3451b5db55dbdbf19bdd15f77d3093
Shelley Roth <shelley.roth@campus.technion.ac.il>
2026-06-17 08:50:54 +0300

    Implmement methods 4.3.6 till 4.3.8

--- wet/Matrix.cpp
+++ wet/Matrix.cpp
@@ -1,17 +1,10 @@
 #include "Matrix.h"
 #include "Utilities.h"
 
-Matrix::Matrix(int n, int m, int initial){
+Matrix::Matrix(int n, int m, int initial) {
     mat_ptr = new int[len];
     for(int i = 0; i < len - 1; i++){
         mat_ptr[i] = initial;
-    }
-}
-
-Matrix::Matrix(int n, int m){
-    mat_ptr = new int[len];
-    for(int i = 0; i < len - 1; i++){
-        mat_ptr[i] = 0;
     }
 }
 
@@ -20,12 +13,46 @@ Matrix::Matrix() : n(0), m(0){}
 Matrix::Matrix(const Matrix& mat){
     n = mat.n;
     m = mat.m;
+    len = mat.len;
     mat_ptr = new int[len];
+
+    for (int i = 0; i < mat.len; i++) {
+        mat_ptr[i] = mat.mat_ptr[i];
+    }
 }
+
 
 //destructor
 Matrix::~Matrix(){
     delete[] mat_ptr;
+}
+
+/*
+ * Assignment Operator
+ * Must do the following:
+ * Delete the old aray, save the array's new size, allocate new memory for
+ * the array. Finally, copy all of the passed matrix's elements into the
+ * allocated memory. Then return a reference to the new matrix.
+ */
+
+Matrix& Matrix::operator=(const Matrix& mat) {
+    if (this ==  &mat) {
+        return *this;
+    }
+
+    delete[] mat_ptr;
+
+    m = mat.m;
+    n = mat.n;
+    len = mat.len;
+
+    mat_ptr = new int[len];
+
+    for (int i = 0; i < len; i++) {
+        mat_ptr[i] = mat.mat_ptr[i];
+    }
+    return *this;
+
 }
 
 int Matrix::getN(){
@@ -53,15 +80,140 @@ void Matrix::setMatElement(int i, int j, int new_value){
     mat_ptr[element] = new_value;
 }
 
-Matrix Matrix::operator+(const Matrix& mat){
-
-    for(int i = 0; i < len - 1; i++){
-        this->mat_ptr[i] += mat.mat_ptr[i];
+Matrix Matrix::operator-() const {
+    for (int i = 0; i < len; i++) {
+        this->mat_ptr[i] *= -1;
     }
 
-    return mat;
+    return *(this);
+}
+
+Matrix& Matrix::operator+=(const Matrix &mat) {
+    if (this -> n != mat.n || this -> m != mat.m) {
+        exitWithError(MatamErrorType::UnmatchedSizes);
+    }
+
+    for (int i = 0; i < len; i++) {
+        this -> mat_ptr[i] += mat.mat_ptr[i];
+    }
+
+    return *this;
+}
+
+Matrix Matrix::operator+(const Matrix& mat) const {
+
+    // Create a copy matrix of *this with dimensions n x m
+    Matrix new_mat(*this);
+
+
+    // += also checks that the dimensions match as required
+    return (new_mat += mat);
 
 }
+
+Matrix& Matrix::operator*=(int scalar) {
+
+    for (int i = 0; i < len; i++) {
+        this -> mat_ptr[i] *= scalar;
+    }
+
+    return *this;
+}
+
+Matrix& Matrix::operator*=(const Matrix &mat) {
+    if (this -> m != mat.n) {
+        exitWithError(MatamErrorType::UnmatchedSizes);
+    }
+
+    Matrix product_mat(m, mat.getN());
+
+    for (int i = 0; i < n; i++) { // for all the (*rows)
+
+
+        for (int j = 0; j < mat.getM(); j++) { // for all the columns of mat
+            // The sum is for a single element in the product matrix. Reset every (i,j)
+            double element_sum = 0;
+            for (int k = 0; k < m; k++) { // for all the columns of (*this)
+                /*
+                 * Based on the product matrix formula which is the sum
+                 * from k = 1 to n of a(i,k) * b(k,j)
+                 */
+                element_sum += (*this)(i, k) * mat(k,j);
+            }
+
+            product_mat(i,j) = element_sum;
+        }
+
+    }
+
+    (*this) = product_mat;
+    return (*this);
+}
+
+
+
+Matrix& Matrix::operator-=(const Matrix &mat) {
+    if (this -> n != mat.n || this -> m != mat.m) {
+        exitWithError(MatamErrorType::UnmatchedSizes);
+    }
+
+    for (int i = 0; i < len; i++) {
+        this -> mat_ptr[i] -= mat.mat_ptr[i];
+    }
+
+    return *this;
+}
+
+
+
+Matrix Matrix::operator-(const Matrix& mat) const{
+
+    return (*this) + (-mat);
+
+}
+
+bool Matrix::operator==(const Matrix& mat) const {
+    if (this->n != mat.getN() || this->m != mat.getM()) {
+        exitWithError(MatamErrorType::UnmatchedSizes);
+    }
+    for (int i = 0; i < len; i++) {
+        if (mat_ptr[i] != mat.mat_ptr[i]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+bool Matrix::operator!=(const Matrix& mat) const {
+    return !((*this) == mat);
+}
+
+Matrix Matrix::rotateClockwise() {
+    Matrix rotatedMatrix(this->m, this->n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            rotatedMatrix(j,n - 1 -i) = (*this)(i,j);
+        }
+    }
+    return rotatedMatrix;
+}
+
+Matrix Matrix::rotateCounterClockwise() {
+    Matrix rotatedMatrix(this->m, this->n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            rotatedMatrix(n - 1 - j,i) = (*this)(i,j);
+        }
+    }
+    return rotatedMatrix;
+}
+
+
+
 
 
 
--- wet/Matrix.h
+++ wet/Matrix.h
@@ -4,47 +4,58 @@
 
 class Matrix{
     int n; //Rows of matrix
-    int m; //Cullomns of matrix
+    int m; //Columns of matrix
     int len = n * m;
     int *mat_ptr = nullptr;
 
     public:
-        //without third argument
-        Matrix(int n, int m, int initial);
-        //with third argument
-        Matrix(int n, int m);
+        Matrix(int n, int m, int initial = 0);
         //default constructor
         Matrix();
+
+    //*********** The Big Three ************
 
         //copy constructor - const because we don't want to change the passed matrix
         Matrix(const Matrix& mat);
 
         //destructor
         ~Matrix();
-        
 
-        //get/set functions for Matrix variables
-        int getN();
-        int getM();
+        // Assignment operator - make a new matrix a reference to the passed matrix
+        Matrix& operator=(const Matrix& mat);
+
+
+        //getter and setter functions for Matrix variables
+        int getN() const;
+        int getM() const;
         int* getMatPtr();
 
-        //get/set functions for elements
+        //getter and setter functions for elements
         int getMatElement(int i, int j);
         void setMatElement(int i, int j, int new_value);
 
+        // rotation
+        Matrix rotateClockwise(); // no parameters because it rotates the current matrix
+        Matrix rotateCounterClockwise(); // no parameters because it rotates the current matrix
+
         // operator overloading
-        Matrix operator+(const Matrix& mat); // add two matrices
-        Matrix operator-(const Matrix& mat); // minus two matrices
-        Matrix operator*(const Matrix& mat); // times
-        Matrix operator+=(const Matrix& mat); // plus equals
-        Matrix operator-=(const Matrix& mat); // minus equals
-        Matrix operator*=(const Matrix& mat); // times equals
-        Matrix operator-();  //unitary minus
+
+        Matrix operator+(const Matrix& mat) const; // add two matrices
+        Matrix operator-(const Matrix& mat) const; // minus two matrices
+        Matrix operator*(const Matrix& mat) const; // times
+        Matrix operator*(int scalar) const; // matrix times scalar
+        Matrix& operator+=(const Matrix& mat); // plus equals
+        Matrix& operator-=(const Matrix& mat); // minus equals
+        Matrix& operator*=(const Matrix& mat); // times equals
+        Matrix operator-() const;  //unary minus
+
+        bool operator==(const Matrix& mat) const; // checks if matrices are equal
+        bool operator!=(const Matrix& mat) const; // checks if matrices are not equal
 
         // double sided multiplcation of a scalar and a matrix
         friend Matrix operator*(int scalar, const Matrix& mat); // scalar times matrix
-      
-        Matrix operator*=(int scalar); // matrix times scalar
+
+        Matrix& operator*=(int scalar); // matrix times scalar
 
 
 };
